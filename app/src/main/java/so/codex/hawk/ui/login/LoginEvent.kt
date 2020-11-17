22141d50c6c1d685d279adfb8a709619575d4af4
temnik <temnik15@bk.ru>
2020-11-17 03:21:26 +0300

    #25: Added documentation.

--- app/src/main/java/so/codex/hawk/ui/login/LoginEvent.kt
+++ app/src/main/java/so/codex/hawk/ui/login/LoginEvent.kt
@@ -1,7 +1,24 @@
 package so.codex.hawk.ui.login
 
+/**
+ * The class is an enumeration of various events that can occur during login.
+ */
 enum class LoginEvent {
+    /**
+     * @property INTERNET_ERROR An event that occurs when the Internet is bad or completely absent.
+     */
     INTERNET_ERROR,
+
+    /**
+     * @property LOGIN_ERROR An event that is raised when an error occurs on the server side.
+     *                       For example, the user entered an incorrect email or password.
+     *                       The server error will be reformatted into this event.
+     */
     LOGIN_ERROR,
+
+    /**
+     * @property SUCCESSFUL_LOGIN An event that occurs when the server confirms
+     *                            a successful authorization.
+     */
     SUCCESSFUL_LOGIN
 }
--- app/src/main/java/so/codex/hawk/ui/login/LoginViewModel.kt
+++ app/src/main/java/so/codex/hawk/ui/login/LoginViewModel.kt
@@ -1,44 +1,80 @@
 package so.codex.hawk.ui.login
 
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
 import so.codex.hawk.domain.login.LoginInteractor
 import so.codex.hawk.domain.login.LoginInteractorImpl
 import so.codex.hawk.entity.UserAuthData
 import timber.log.Timber
 
+/**
+ * The ViewModel class for LoginActivity.
+ * Designed to handle various processes associated with the specified activity.
+ */
 class LoginViewModel : ViewModel() {
+    /**
+     * @property loginInteractor Interactor for handling authorization logic.
+     */
     private val loginInteractor: LoginInteractor = LoginInteractorImpl()
+
+    /**
+     * @property loginEvent A LiveData object to reactively pass a LoginEvent to the View.
+     * @see LiveData
+     */
     private val loginEvent = MutableLiveData<LoginEvent>()
+
+    /**
+     * @property loginEventDisposable field for saving the [Disposable] object,
+     *                                which is required to unsubscribe from LoginEventObservable.
+     */
     private lateinit var loginEventDisposable: Disposable
 
-
+    /**
+     * Initialization block. Called on creation.
+     */
     init {
         subscribeLoginInteractor()
     }
 
-
+    /**
+     * Authorization method. ViewModel delegates [LoginInteractor] execution.
+     *
+     * @param email user email.
+     *
+     * @param password user password.
+     */
     fun login(email: String, password: String) {
         loginInteractor.login(UserAuthData(email, password))
     }
 
+    /**
+     * Method to provide LiveData [loginEvent] for observers.
+     */
     fun observeLoginEvent(): LiveData<LoginEvent> {
         return loginEvent
     }
 
+    /**
+     * Method for subscribing to results of execution [loginInteractor]
+     */
     private fun subscribeLoginInteractor() {
         loginEventDisposable =
-            loginInteractor.getLoginEventObservable().observeOn(AndroidSchedulers.mainThread())
+            loginInteractor
+                .getLoginEventObservable()
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
                     loginEvent.value = it
                 }
     }
 
+    /**
+     * The method that will be called when the activity to which the ViewModel belongs is destroyed.
+     * Needed to clean up used resources.
+     * @see [LoginActivity]
+     */
     override fun onCleared() {
         loginEventDisposable.dispose()
         Timber.i("LoginEvent dispose!")
